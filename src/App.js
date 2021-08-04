ded5b3d65e9ec74aaefa19fcacbb913a13eb4730
Majd Kayyal <majd.kayyal.3398@hotmail.com>
2021-08-04 20:22:11 +0200

    defining the home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './header-components/Header.js';
+import Home from './home-components/Home.js';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
 
     </div>
   );
