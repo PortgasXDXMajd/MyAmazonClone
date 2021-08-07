a8c38195b41d2f8a693b6b15b59dc75a023f4f7a
Majd Kayyal <majd.kayyal.3398@hotmail.com>
2021-08-07 22:09:05 +0200

    adding checkout page

--- src/payment-cpmponent/PaymentPage.js
+++ src/payment-cpmponent/PaymentPage.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import './PaymentPage.css'
+import { useStateValue } from '../state-provider/StateProvider';
+import CheckoutProduct from '../checkout-components/CheckoutProduct';
 
 function PaymentPage() {
+    const [{basket ,user},] = useStateValue();
     return (
         <div className="paymentPage">
-            
+            <h1>Checkout({basket?.length} items)</h1>
+            <div className="paymentPage_Container">
+                <div className="paymentPage_section">
+                    <div className="paymentPage_LeftSection">Delivery Address</div>
+                    <div className="paymentPage_RightSection">
+                        <p>{user?.email}</p>
+                        <p>Am Weissenberg 18 APP 122</p>
+                        <p>Aachen Germany</p>
+                    </div>
+
+                </div>
+                <div className="paymentPage_section">
+                    <div className="paymentPage_LeftSection">Review Items and delivery</div>
+                    <div className="paymentPage_RightSection">
+                        <>
+                            {basket.map((item)=>(
+                                <CheckoutProduct product={item}/>
+                            ))}
+                        </>
+                    </div>
+
+                </div>
+                <div className="paymentPage_section">
+                    <div className="paymentPage_LeftSection">Payment Method</div>
+                    <div className="paymentPage_RightSection">
+                        <h3>Card Details</h3>
+                        <form>
+                            <div className="paymentPage_RightSection_FormField">
+                                <input type="number" hint="Card number"/>
+                                <input type="date" hint="MM/YY"/>
+                                <input type="number" hint="CVC"/>
+                            </div>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
