d91719fbb12636434639874347f50986f7682a0f
Majd Kayyal <majd.kayyal.3398@hotmail.com>
2021-08-07 16:53:09 +0200

    adding

--- src/authentication-components/LoginPage.js
+++ src/authentication-components/LoginPage.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LoginPage.css';
 import {Link} from 'react-router-dom';
 
 function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const signIn = (e) => {
+        e.preventDefault();
+        console.log({email})
+        console.log({password})
+
+    };
+    const register = (e) => {
+        e.preventDefault();
+        console.log('Register')
+
+    };
+
+
     return (
         <div className="loginPage">
             <Link to="/">
@@ -14,16 +29,16 @@ function LoginPage() {
                 <h1 className="loginPage_ContainerTitle">Sign In</h1>
                 <form className="loginPage_Form">
                     <h5>E-mail</h5>
-                    <input type='text'/>
+                    <input type='text' value={email} onChange={(e)=>setEmail(e.target.value)}/>
                     <h5>Password</h5>
-                    <input type='password'/>
-                    <button className="loginPage_SignInButton">SIGN IN</button>
+                    <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                    <button className="loginPage_SignInButton" type="submit" onClick={signIn}>SIGN IN</button>
                 </form>
                 <p>
                     By continuing, you agree to Amazon's FAKE CLONE
                     Conditions of Use and Privacy Notice.
                 </p>
-                <button className="loginPage_CreateButton">CREATE AMAZON ACCOUNT</button>
+                <button className="loginPage_CreateButton" onClick={register}>CREATE AMAZON ACCOUNT</button>
             </div>
         </div>
     )
