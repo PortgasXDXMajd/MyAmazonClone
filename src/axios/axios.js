a4be31aa0ba0bc9dbab1ea600f4c36c4806af573
Majd Kayyal <majd.kayyal.3398@hotmail.com>
2021-08-09 00:29:20 +0200

    project is finished

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/myamazomclone/us-central1/api'
+    baseURL: 'https://us-central1-myamazomclone.cloudfunctions.net/api'
+    //'http://localhost:5001/myamazomclone/us-central1/api'
 });
 
 
